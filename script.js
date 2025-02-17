154e8f5164083964e7ab16cac5e1bb4f04e4dc25
uTelyK <166298064+uTelyK@users.noreply.github.com>
2025-02-17 23:21:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('nutCopylink').addEventListener('click', function() {
+    const link = window.location.href;
+    navigator.clipboard.writeText(link).then(function() {
+ 
+        const button = document.getElementById('nutCopylink');
+        const originalText = button.textContent;
+        button.textContent = 'Copied';
+    // debounce cua copy button set bang 2 giay
+        setTimeout(function() {
+            button.textContent = originalText;
+        }, 2000);
+    // debug code khi ma khong the copy ra link
+    }).catch(function(err) {
+        console.error('khong the copy: ', err);
+    });
+});
